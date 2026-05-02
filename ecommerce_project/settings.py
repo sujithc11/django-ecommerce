5c236c48c3c57f6e22e1df44cb126dd0eb5f1cf7
sujithc11 <sujithc110411@gmail.com>
2026-05-02 12:17:05 +0530

    fixed postgres database

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -76,13 +76,16 @@ WSGI_APPLICATION = 'ecommerce_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
+import os
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
