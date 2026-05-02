ac7eb5d7ac179587d5e2052767603a5b91156bb1
sujithc11 <sujithc110411@gmail.com>
2026-05-02 09:36:53 +0530

    added gitignore

--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('vendor/', views.vendor_dashboard, name='vendor_dashboard'),
-    path('login/', views.login_user, name='login'),   # ✅ important
+    path('login/', views.login_user, name='login'),   
     path('logout/', views.logout_user, name='logout'),
     path('signup/', views.signup, name='signup'),
     path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
